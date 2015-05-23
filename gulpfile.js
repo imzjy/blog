4f4007fc61df73345e0cf061559aed75523ed531
zjy <imjatsz@gmail.com>
2015-05-23 17:45:01 +0800

    using gulp to minify the html

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var minify_html = require('gulp-minify-html');
+ 
+gulp.task('minify_html', function() {
+  var opts = {
+    conditionals: true,
+    spare:true
+  };
+ 
+  return gulp.src('src/*.html')
+    .pipe(minify_html(opts))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('static', function(){
+	return gulp.src(['src/robots.txt','src/favicon.ico'])
+		.pipe(gulp.dest('dist/'));
+})
+
+
+gulp.task('default',['minify_html', 'static'],function(){
+	console.log("finished!");
+})
+
