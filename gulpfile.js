f658455fe7c8c311a6470854b935176e3efb4a7d
zjy <imjatsz@gmail.com>
2015-06-01 07:54:01 +0800

    add build target for css and font

--- gulpfile.js
+++ gulpfile.js
@@ -15,9 +15,19 @@ gulp.task('minify_html', function() {
 gulp.task('static', function(){
 	return gulp.src(['src/robots.txt','src/favicon.ico'])
 		.pipe(gulp.dest('dist/'));
-})
+});
+
+gulp.task('css', function(){
+  gulp.src('src/css/*.css')
+    .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('font', function(){
+    gulp.src('src/fonts/*.*')
+    .pipe(gulp.dest('dist/fonts/'));
+});
 
 
-gulp.task('default',['minify_html', 'static'],function(){
+gulp.task('default',['minify_html', 'static', 'css', 'font'],function(){
 	console.log("finished!");
-})
+});
